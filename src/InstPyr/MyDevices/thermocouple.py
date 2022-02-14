c0ad4cfb3c4ccdae1fd722de602421cf53851dbd
ssood <Sol97Sat25#Resp!>
2022-02-14 10:08:18 -0800

    Added ability to calibrate an thermocouple (linear interpolation)

--- src/InstPyr/MyDevices/thermocouple.py
+++ src/InstPyr/MyDevices/thermocouple.py
@@ -1,10 +1,14 @@
 from ..Interfaces.Common import interface,inf_enums
+from scipy import interpolate
 class thermocouple():
     def __init__(self,interface:interface.interface,pins, type='k', units=inf_enums.TempUnits.CELSIUS):
         self.interface=interface
         self.interface.initializeIO(ain=[pins])
         self.pins=pins
         self.type=type
+        self.calX=[]
+        self.calY=[]
+        self.calfunc=None
         self.currentTemp=0
         if(self.interface.configThermocouple(pins,type)==None):
             self.config(type)
@@ -12,10 +16,20 @@ class thermocouple():
 
     def readTemperature(self):
         self.currentTemp=round((self.interface.readTemperature(self.pins,self.units)),3)
+        self.currentTemp=self._applyCalibration(self.currentTemp)
         if(self.currentTemp==None):
             return self.calcTemp()
         else:
             return self.currentTemp
+
+    def _applyCalibration(self, rawTemp):
+        if self.calfunc is not None:
+            return float(self.calfunc(rawTemp))
+
+    def calibrate(self,Xin,Yin):
+        self.calX=Xin
+        self.calY=Yin
+        self.calfunc=interpolate.interp1d(self.calX,self.calY)
 
     def config(self,type):
         pass
