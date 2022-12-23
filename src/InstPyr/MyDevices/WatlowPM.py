07859eeb160d7676ac608163308a2e579e9654a7
soodsidd@gmail.com <707micromachine>
2022-12-22 17:36:53 -0800

    Added support for watlow PM controllers (based on MODBUS TCP/IP)

--- src/InstPyr/MyDevices/WatlowPM.py
+++ src/InstPyr/MyDevices/WatlowPM.py
@@ -7,6 +7,10 @@ CURRENT_SETPOINT_ADDR=2160
 COOLPOWER=1906
 HEATPOWER=1904
 SETPOINTLIMITS=(-20,70)
+CONTROLMODE=1880
+OFFVAL=62
+AUTOVAL=10
+MANVAL=54
 
 
 class WatlowPM_TCP():
@@ -40,10 +44,14 @@ class WatlowPM_TCP():
 
     def writeSetpoint(self,setpoint):
         if setpoint>=SETPOINTLIMITS[0] and setpoint<=SETPOINTLIMITS[1]:
-            res=self._convertToBits(setpoint)
-            hi=int(res[0:16].bin.encode('ascii'),2)
-            lo=int(res[16:].bin.encode('ascii'),2)
-            self.client.write_registers(CURRENT_SETPOINT_ADDR,[lo,hi])
+            bits=self._convertFloatToBits(setpoint)
+            self._writeLoHiRegisters(CURRENT_SETPOINT_ADDR,bits)
+
+
+    def _writeLoHiRegisters(self,addr,bits):
+        hi = int(bits[0:16].bin.encode('ascii'), 2)
+        lo = int(bits[16:].bin.encode('ascii'), 2)
+        self.client.write_registers(addr, [lo, hi])
 
 
     def _convertToFloat(self,bits):
@@ -51,9 +59,25 @@ class WatlowPM_TCP():
         final = struct.unpack('>f', s)[0]
         return final
 
-    def _convertToBits(self,num):
+    def _convertFloatToBits(self,num):
         f1=bitstring.BitArray(float=num,length=32)
         return f1
+
+    def _convertIntToBits(self,num):
+        f1=bitstring.BitArray(uint=num,length=32)
+        return f1
+
+    def SetControlMode(self,mode):
+        val=None
+        if mode==0:
+            val=OFFVAL
+        elif mode==1:
+            val=AUTOVAL
+        if val is not None:
+            bits=self._convertIntToBits(val)
+            self._writeLoHiRegisters(CONTROLMODE,bits)
+
+
 
     def close(self):
         self.client.close()
